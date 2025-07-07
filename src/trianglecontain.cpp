5e6d63b8aa89db9f5bf1a67ac40be84a2a39f6c5
Robby Lawrence <46226451+RobbyLawrence@users.noreply.github.com>
2025-07-07 13:37:55 -0400

    Add origin containment check

--- src/trianglecontain.cpp
+++ src/trianglecontain.cpp
@@ -7,17 +7,34 @@ using namespace std;
 struct Point {
     int x;
     int y;
-    Point();
+
+    Point() : x(0), y(0) {}
     Point(int x_n, int y_n) : x(x_n), y(y_n) {}
 };
 
 struct Triangle {
-    tuple<Point, Point, Point> points;
-    Triangle(Point a, Point b,) {
+    Point a;
+    Point b;
+    Point c;
 
-        this->points = make_tuple(a,b);
-    }
+    Triangle(Point a_n, Point b_n, Point c_n) : a(a_n), b(b_n), c(c_n) {}
 };
+
+static int sign(const Point &p1, const Point &p2, const Point &p3) {
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+bool contains_origin(const Triangle &t) {
+    Point origin(0, 0);
+    int d1 = sign(origin, t.a, t.b);
+    int d2 = sign(origin, t.b, t.c);
+    int d3 = sign(origin, t.c, t.a);
+
+    bool has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    bool has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(has_neg && has_pos);
+}
 
 int main() {
 
